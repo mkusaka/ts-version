16efe824ade87276e7dcfebed1d43a719b4e037e
mkusaka <hinoshita1992@gmail.com>
2020-03-16 22:39:48 +0900

    add ts324

---
+++ ts324/src/index.ts
@@ -0,0 +1,8 @@
+export const sum = (a: number, b: number) => {
+  if ('development' === process.env.NODE_ENV) {
+    console.log('boop');
+  }
+  return a + b;
+};
+
+export const fmap = [1, 2, 3].flatMap(e => e.toString());
